e951fd352df46094e9a0ad796f643e51ccd5ab99
Andile <andile.goba@younglings.africa>
2022-11-09 04:25:32 +0200

    react icon change update commit

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RiProductHuntLine } from "react-icons/ri";
+import { SiApache } from "react-icons/si";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 import heroImg from "../../assets/inv-img.png";
@@ -10,7 +10,7 @@ const Home = () => {
     <div className="home">
       <nav className="container --flex-between ">
         <div className="logo">
-          <RiProductHuntLine size={35} />
+          <SiApache size={45} />
         </div>
 
         <ul className="home-links">
